e0cb30ca2a1e69aa1a249a6dd814c58f25462bee
Luis <lfierro743@cbcsd.org>
2024-12-12 16:52:06 -0600

    Pre-development stage update

---
+++ src/main/java/com/xojangstudios/eaglecrafty/Game.java
@@ -0,0 +1,11 @@
+package com.xojangstudios.eaglecrafty;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EaglecraftY v" + getVersion());
+    }
+
+    public static String getVersion() {
+        return "0.0-PreDevelopment";
+    }
+}
