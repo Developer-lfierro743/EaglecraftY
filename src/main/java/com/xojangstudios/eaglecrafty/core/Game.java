8dd6517fee8bfb166689bfe2fa2ae15fcf4451b8
Developer-lfierro743 <lfierro743@cbcsd.org>
2024-12-31 09:10:44 -0600

    intergrated the renderer into the game class

--- src/main/java/com/xojangstudios/eaglecrafty/core/Game.java
+++ src/main/java/com/xojangstudios/eaglecrafty/core/Game.java
@@ -1,62 +1,23 @@
 package com.xojangstudios.eaglecrafty.core;
 
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.glfw.GLFW;
+import com.xojangstudios.eaglecrafty.engine.Renderer;
 import com.xojangstudios.eaglecrafty.engine.Window;
 
 public class Game {
-    private boolean running = false;
-    private double lastTime = 0;
-    private double deltaTime = 0;
+    private Renderer renderer;
 
     public void start() {
-        init();
-        running = true;
-        run();
-    }
-
-    private void init() {
         Window.init();
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    }
+        renderer = new Renderer();
+        renderer.init();
 
-    private void run() {
-        lastTime = GLFW.glfwGetTime();
-
-        while (running && !Window.shouldClose()) {
-            double currentTime = GLFW.glfwGetTime();
-            deltaTime = currentTime - lastTime;
-            lastTime = currentTime;
-
-            input();
-            update(deltaTime);
-            render();
-
+        while (!Window.shouldClose()) {
+            renderer.render();
             Window.swapBuffers();
             Window.pollEvents();
         }
 
-        cleanup();
-    }
-
-    private void input() {
-        if (GLFW.glfwGetKey(Window.getWindow(), GLFW.GLFW_KEY_ESCAPE) == GLFW.GLFW_PRESS) {
-            running = false;
-        }
-    }
-    
-    private void update(double deltaTime) {
-        // Update game logic here
-        System.out.println("Delta time: " + deltaTime);
-    }
-
-    private void render() {
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-        // Render game objects here
-    }
-
-    private void cleanup() {
+        renderer.cleanup();
         Window.terminate();
     }
 
