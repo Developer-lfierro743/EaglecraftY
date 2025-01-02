8584dd4ce04e0af29ac1db716011a9e3d6d36986
Developer-lfierro743 <lfierro743@cbcsd.org>
2025-01-02 04:18:33 -0600

    fixed and added update() in World.java file

--- src/main/java/com/xojangstudios/eaglecrafty/world/World.java
+++ src/main/java/com/xojangstudios/eaglecrafty/world/World.java
@@ -6,4 +6,9 @@ public class World {
         // Initialize the world
         System.out.println("World initialized");
     }
+
+    public void update() {
+        // Add logic to update the world state
+        System.out.println("World updated!");
+    }
 }
